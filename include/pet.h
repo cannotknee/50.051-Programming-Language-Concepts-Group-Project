ecc63c367ed06c43767ff20688f7ffa55fd4ea5a
Lee Darren <hecatehonryuu@gmail.com>
2024-03-25 14:05:25 +0800

    minor comments

--- include/pet.h
+++ include/pet.h
@@ -1,3 +1,4 @@
+/* BEGIN: abpxx6d04wxr */
 #ifndef _HAVE_STAT
 #define _HAVE_STAT
 
@@ -11,46 +12,65 @@ typedef enum {
     GOOD_STATE = 3
 } state;
 
+
+/*Cleanliness: independent of other stats, affected by play/train/bathe
+Fatigue: affected by health, affected by play/train/bathe/sleep
+Hunger: independent of other stats, affected by feed
+Health: affected by cleanliness, affected by medicine/vet
+Happiness: affected by everything, affected by play*/
 typedef enum {
     STAT_GROWTH = 0,
-    STAT_CLEANLINESS = 1,
-    STAT_FATIGUE = 2,
-    STAT_HAPPINESS = 3,
-    STAT_HEALTH = 4,
+    STAT_HAPPINESS = 1,
+    STAT_HEALTH = 2,
+    STAT_CLEANLINESS = 3,
+    STAT_FATIGUE = 4,
     STAT_HUNGER = 5
 } stat;
 
+/*Feed: hunger, health, fatigue
+Play: happiness, health, fatigue
+Bathe: happiness, health
+Train: everything
+Sleep: fatigue, health
+Medicine: health
+Vet: nothing */
 typedef enum {
     ACTION_FEED,
     ACTION_PLAY,
     ACTION_BATHE,
     ACTION_TRAIN,
     ACTION_SLEEP,
-    ACTION_MEDICINE
+    ACTION_MEDICINE,
+    ACTION_VET
 } action;
 
 typedef struct lepet {
-    char* name; //Up to 20 characters
+    char* name; /*Up to 20 characters*/
     stat* stat_name;
-    state* stat_state; //stat
-    double* multiplier; //for calculations of state transition, basically personality
-    int* since_last_change; //turns since last change
+    state* stat_state; /*stat*/
+    double* multiplier; /*for calculations of state transition, basically personality*/
+    double* offsets; /*for update_stat, calculated based on current state of other stats*/
+    int* since_last_change; /*turns since last change*/
 } pet;
 
-//malloc for pet attributes and init everything but name
+/*malloc for pet attributes and init everything but name*/
 void init_pet(pet* p);
 
-//free pet attributes and set p to nullptr
+/*free pet attributes and set p to nullptr*/
 void free_pet(pet* p);
 
-//Update specified stat, offset provided by other conditions such as state of other stats
+/*Update specified stat except growth, offset provided by other conditions such as state of other stats*/
 void update_stat(state* s, int since_last_change, double multiplier, float offset);
 
-//Update all stats on turn end
-void update_stats(pet* p);
+/*Look at other stats and update offsets*/
+void update_offsets(pet* p)
 
-//calculate success of action and update stats accordingly
-//Return 0 if action failed, 1 if action succeeded, 2 if action was super successful
+/*Update all stats on turn end*/
+/*Includes logic to look at other states and input offset accordingly*/
+void update__all_stats(pet* p);
+
+/*calculate success of action and update stats accordingly*/
+/*Return 0 if action failed, 1 if action succeeded, 2 if action was super successful*/
 int handle_action(pet* p, action a);
 
 
--- src/pet.c
+++ src/pet.c
@@ -1,4 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
 
+#include "pet.h"
 
-#include "status.h"
+void init_pet(pet* p) {
+    int i;
 
+    p->name = (char*)malloc(NAME_LENGTH * sizeof(char));
+    p->stat_name = (stat*)malloc(STAT_COUNT * sizeof(stat));
+    p->stat_state = (state*)malloc(STAT_COUNT * sizeof(state));
+    p->multiplier = (double*)malloc(STAT_COUNT * sizeof(double));
+    p->since_last_change = (int*)malloc(STAT_COUNT * sizeof(int));
+    for (i = 0; i < STAT_COUNT; i++) {
+        p->stat_name[i] = (stat)i;
+        p->stat_state[i] = NORMAL_STATE;
+        p->multiplier[i] = 0.1; /*Currently just set all to 0.1, will need to somehow randomise this or use template in future*/
+        p->since_last_change[i] = 0;
+    }
+}
+
+void free_pet(pet* p) {
+    free(p->name);
+    free(p->stat_name);
+    free(p->stat_state);
+    free(p->multiplier);
+    free(p->since_last_change);
+    p = NULL;
+}
+
+void update_stat(state* s, int since_last_change, double multiplier, float offset) {
+    double randval;
+    randval = rand() / RAND_MAX; /*Rand val btwn 0 & 1*/
+    if (randval < since_last_change * multiplier + offset && *s >0) {
+        *s = *s - 1;
+    }
+    /*Danger state will be handled in wrapper func update_stats*/
+}
